7cff1cc1d2b27348f5fad684b8da187acf1705a3
Diss <Disslove77777@gmail.com>
2016-02-14 09:11:40 -0800

    Update channels.lua

--- plugins/channels.lua
+++ plugins/channels.lua
@@ -1,37 +1,37 @@
 -- Checks if bot was disabled on specific chat
-local function is_channel_disabled( receiver )
-	if not _config.disabled_channels then
+local function is_channel_dis( receiver )
+	if not _config.dis_channels then
 		return false
 	end
 
-	if _config.disabled_channels[receiver] == nil then
+	if _config.dis_channels[receiver] == nil then
 		return false
 	end
 
-  return _config.disabled_channels[receiver]
+  return _config.disa_channels[receiver]
 end
 
-local function enable_channel(receiver)
-	if not _config.disabled_channels then
+local function en_channel(receiver)
+	if not _config.dis_channels then
 		_config.disabled_channels = {}
 	end
 
-	if _config.disabled_channels[receiver] == nil then
+	if _config.dis_channels[receiver] == nil then
 		return 'Channel isn\'t disabled'
 	end
 	
-	_config.disabled_channels[receiver] = false
+	_config.dis_channels[receiver] = false
 
 	save_config()
 	return "Channel re-enabled"
 end
 
 local function disable_channel( receiver )
-	if not _config.disabled_channels then
-		_config.disabled_channels = {}
+	if not _config.dis_channels then
+		_config.dis_channels = {}
 	end
 	
-	_config.disabled_channels[receiver] = true
+	_config.dis_channels[receiver] = true
 
 	save_config()
 	return "Channel disabled"
@@ -42,12 +42,12 @@ local function pre_process(msg)
 	
 	-- If sender is sudo then re-enable the channel
 	if is_sudo(msg) then
-	  if msg.text == "!channel enable" then
-	    enable_channel(receiver)
+	  if msg.text == "!channel en" then
+	    ena_channel(receiver)
 	  end
 	end
 
-  if is_channel_disabled(receiver) then
+  if is_channel_dis(receiver) then
   	msg.text = ""
   end
 
@@ -57,11 +57,11 @@ end
 local function run(msg, matches)
 	local receiver = get_receiver(msg)
 	-- Enable a channel
-	if matches[1] == 'enable' then
+	if matches[1] == 'en' then
 		return enable_channel(receiver)
 	end
 	-- Disable a channel
-	if matches[1] == 'disable' then
+	if matches[1] == 'dis' then
 		return disable_channel(receiver)
 	end
 end
@@ -69,11 +69,11 @@ end
 return {
 	description = "Plugin to manage channels. Enable or disable channel.", 
 	usage = {
-		"!channel enable: enable current channel",
-		"!channel disable: disable current channel" },
+		"!channel en: enable current channel",
+		"!channel dis: disable current channel" },
 	patterns = {
-		"^!channel? (enable)",
-		"^!channel? (disable)" }, 
+		"^!channel? (en)",
+		"^!channel? (dis)" }, 
 	run = run,
 	privileged = true,
 	pre_process = pre_process
